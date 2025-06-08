fa50245278a1ab2a6f11d724b4e3fb5a76b174e1
Gustaf Mossakowski <gustaf@koenige.org>
2025-06-08 23:56:52 +0200

    move balance text function to functions

--- zzbrick_request/certificate.inc.php
+++ zzbrick_request/certificate.inc.php
@@ -326,45 +326,6 @@ function mod_certificates_certificate($params, $settings = [], $event = []) {
 	wrap_send_file($file);
 }
 
-/**
- * Balance long text in two lines
- *
- * @param string $verein
- * @param int $max_len
- * @param int $len_per_row
- * @return array
- */
-function mf_certificates_balance_text($text, $max_len, $len_per_row) {
-	if (strlen($text) < $max_len) return [$text];
-	$concat = strstr($text, ', ') ? ', ' : ' ';
-	$parts = explode($concat, $text);
-	$text = [0 => ''];
-	$i = 0;
-	foreach ($parts as $part) {
-		if (strlen($text[$i].$part) > $len_per_row) $i++;
-		if (!empty($text[$i]))
-			$text[$i] .= $concat;
-		else 
-			$text[$i] = '';
-		if (strlen($part) >= $len_per_row AND strstr($part, '-')) {
-			$part = explode('-', $part);
-			foreach ($part as $index => $sub_part) {
-				if (strlen($text[$i].$sub_part) >= $len_per_row) {
-					$i++;
-					$text[$i] = '';
-				}
-				$text[$i] .= $sub_part;
-				if ($index < count($part) - 1)
-					$text[$i] .= '-';
-			}
-		} else {
-			$text[$i] .= $part;
-		}
-	}
-	if (empty($text[0])) array_shift($text); // if first string too long
-	return $text;
-}
-
 function mf_certificates_subtitle_dsm(&$title) {
 	$glue = [];
 	$glue_parts = false;
